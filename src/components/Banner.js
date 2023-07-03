ba20bf2c9e4236e4dc6fb0b03f821b79ccbd94de
IT21440236 <it21440236@my.sliit.lk>
2023-07-03 14:47:16 +0530

    updates

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -86,5 +86,3 @@ const Banner = () => {
 };
 
 export default Banner;
-
-//commit check1
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -9,7 +9,6 @@ import colorSharp from "../assets/img/color-sharp.png";
 const Skills = () => {
   const responsive = {
     superLargeDesktop: {
-      // the naming can be any, depends on you.
       breakpoint: { max: 4000, min: 3000 },
       items: 5,
     },
